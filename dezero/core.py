f333743b3434ecf9edc0ca30261bfc161ede4b8c
Victor28 <sea810749@gmail.com>
2026-08-07 22:00:29 +0800

    feat: Type Hints

--- dezero/core.py
+++ dezero/core.py
@@ -2,29 +2,29 @@ import numpy as np
 
 
 class Variable:
-    def __init__(self, data):
+    def __init__(self, data: np.ndarray):
         self.data = data
 
 class Function:
 
-  def __call__(self, input):
+  def __call__(self, input: Variable) -> Variable:
     x = input.data
     y = self.forward(x)
     output = Variable(y)
     return output
 
-  def forward(self, x):
+  def forward(self, x: np.ndarray) -> np.ndarray:
     raise NotImplementedError()
 
 class Square(Function):
 
-  def forward(self, x):
+  def forward(self, x: np.ndarray) -> np.ndarray:
     return x**2
 
 
 class Exp(Function):
 
-  def forward(self, x):
+  def forward(self, x: np.ndarray) -> np.ndarray:
     return np.exp(x)
   
 
