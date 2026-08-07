56d3feb354caf9f6bff2a06ff6b21c9ac7b37fc3
Victor28 <sea810749@gmail.com>
2026-08-07 15:49:38 +0800

    feat: complete package path test

---
+++ dezero/hello_word.py
@@ -0,0 +1,6 @@
+class test_hello():
+    print("Hello world!")
+
+if __name__ == "__main__":
+    hello = test_hello()
+    hello
---
+++ package_test/test_hello_world.py
@@ -0,0 +1,5 @@
+from dezero.hello_word import test_hello
+
+if __name__ == "__main__":
+    hello = test_hello()
+    hello
